4a571b1b54d8dc330b78a750781f0ac5c50c4c82
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-09 16:01:17 +0545

    Adding app.js with transport

---
+++ app.js
@@ -0,0 +1,10 @@
+const nodemailer = require('nodemailer');
+
+var transport = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USERNAME,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
