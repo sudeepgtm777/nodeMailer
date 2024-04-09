ee9d10293667ea3eb6f27b1135ea0d7ef724a1f7
sudeepgtm777 <sudeepgautam777@gmail.com>
2024-04-09 16:26:57 +0545

    Adding sendEmail function

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const dotenv = require('dotenv');
 const nodemailer = require('nodemailer');
+
+dotenv.config({ path: './config.env' });
+
+console.log(process.env.EMAIL_HOST);
 
 var transport = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
@@ -7,4 +12,17 @@ var transport = nodemailer.createTransport({
     user: process.env.EMAIL_USERNAME,
     pass: process.env.EMAIL_PASSWORD,
   },
+});
+
+const message = {
+  from: 'onlineSudeep',
+  to: 'sudeeponlie@gmail.com',
+  subject: 'How are you',
+  text: 'Testing purpose',
+};
+
+transport.sendMail(message, (err, info) => {
+  if (!err) console.log(info);
+
+  console.log(err);
 });
